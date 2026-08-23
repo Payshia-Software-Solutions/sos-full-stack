dd67231490786b12c71a836f0415b3eca565f08e
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2026-08-23 13:32:06 +0530

    feat: implement student KYC document upload and verification status interface

--- server/routes/Student/studentDocumentVerificationRoutes.php
+++ server/routes/Student/studentDocumentVerificationRoutes.php
@@ -5,7 +5,7 @@ $pdo = $GLOBALS['pdo'];
 $kycController = new StudentDocumentVerificationController($pdo);
 
 return [
-    'GET /student-kyc/status/{studentId}/' => [$kycController, 'getStatusByStudent'],
+    'GET /student-kyc/status/{username}/' => [$kycController, 'getStatusByStudent'],
     'POST /student-kyc/submit/' => [$kycController, 'submitDocuments'],
     'GET /admin/student-kyc/' => [$kycController, 'getAllRecords'],
     'GET /admin/student-kyc/{id}/' => [$kycController, 'getRecordById'],
